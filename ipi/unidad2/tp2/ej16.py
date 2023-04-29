f0b9053d3cc627c2d724f10b27f6a19ac6559cfc
kochq <matukoch@live.com>
2023-04-29 19:52:26 -0300

    Arreglando tp16

--- ipi/unidad2/tp2/ej16.py
+++ ipi/unidad2/tp2/ej16.py
@@ -1,15 +1,16 @@
 contador = 0
 primos = 0
-n = 1
+n = int(input('Ingrese un numero mayor a 1: '))
 
 while n != 0:
-    n = int(input('Ingrese un numero mayor a 1: '))
     for i in range(2, n):
         if n % i == 0:
             contador += 1
+
     if contador == 0:
         primos += 1
     contador = 0
+    n = int(input('Ingrese un numero mayor a 1: '))
 
 print(f'La cantidad de numeros primos fue de {primos}')
 
