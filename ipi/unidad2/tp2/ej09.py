587fe83f6a39b8920e142f498b89fc704083d3e7
kochq <matukoch@live.com>
2023-05-01 13:28:56 -0300

    Completando ej09

--- ipi/unidad2/tp2/ej09.py
+++ ipi/unidad2/tp2/ej09.py
@@ -1,8 +1,17 @@
 cantNum = int(input('¿Que cantidad de numeros desea ingresar?: '))
 suma = 0
+pares = 0
+impares = 0
 
 for i in range(cantNum):
     nuevoNum = int(input('Ingrese el numero: '))
     if nuevoNum >= 0:
         suma += nuevoNum
-print(suma)
+        if nuevoNum % 2:
+            impares += 1
+        else:
+            pares += 1
+
+print(f'La suma total es {suma}')
+print(f'Hay {pares} numeros pares')
+print(f'Hay {impares} numeros impares')
