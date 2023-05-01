b23e349690dbf97321f5f82545034bc89ce07949
kochq <matukoch@live.com>
2023-05-01 13:09:03 -0300

    Arreglando ej07

--- ipi/unidad2/tp2/ej07.py
+++ ipi/unidad2/tp2/ej07.py
@@ -1,7 +1,11 @@
 numero = int(input('Ingrese el numero: '))
 
+i = numero
 if numero % 2:
-    i = numero
     while i != numero*2:
         i+=1
+        print(i)
+else:
+    while i != numero*2:
+        i+=2
         print(i)
