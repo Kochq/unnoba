831190d0e3340bc8ab77389e021c3ed5bb75da03
Mateo Koch <matukoch@live.com>
2023-06-21 11:25:56 -0300

    Update ej10.py

--- ipi/unidad4/tp4/ej10.py
+++ ipi/unidad4/tp4/ej10.py
@@ -2,11 +2,11 @@ def ocurrencias(lista):
     newArray = []
 
     for i in lista:
-        ocurrencia = 1 # Lo inicio en 1 porque si no la resolucion del ejercicio no es igual, pero creo que esta mal redactado, si la z no tiene otra z adyacente el resultado deberia ser [z,0]
-        original = lista.index(i)
+        ocurrencia = 1 # Lo inicio en 1 porque si no la resolucion del ejercicio no es igual, si la z no tiene otra z adyacente el resultado deberia ser [z,0]
+        adyacente= lista.index(i)+1
         limite = len(lista)-1
 
-        for a in lista[original+1:limite]:
+        for a in lista[adyacente:limite]: # Recorro la lista solo en los valores que me interesan
             if a != i:
                 break
             else:
@@ -16,5 +16,3 @@ def ocurrencias(lista):
         lista.pop(0) #Elimino el elemento que acabo de comprobar para no tener repetidos
 
     return newArray
-
-print(ocurrencias(['z', 7, True, True, 34, 'z', 'z', 'z', 3.14]))
