6a1acc7a812281ff18dc657811cbd638a852c14a
kochq <matukoch@live.com>
2023-09-19 15:52:49 -0300

    Terminando el tp2

--- pi/unidad2/tp2/ej27.cpp
+++ pi/unidad2/tp2/ej27.cpp
@@ -1,3 +1,4 @@
+#include <cctype>
 #include <iostream>
 #include <string>
 
@@ -5,27 +6,76 @@ using std::cout;
 using std::cin;
 using std::string;
 using std::ws;
+using std::tolower;
+using std::toupper;
 
 struct concursante {
   string nombre1, nombre2, cancion;
   int numInsc;
 
+  //Constructors
   concursante(string n1, string n2, string c, int n) : nombre1(n1), nombre2(n2), cancion(c), numInsc(n) {};
   concursante() : nombre1(""), nombre2(""), cancion(""), numInsc(0) {};
 };
 
 struct musica {
-  string nombre;
-  int duracion; };
+  string titulo;
+  int duracion; 
+};
 
+string formatearCancion(string cancion);
 void cargaConcursantes(struct concursante *concursantes, short &cantConcursantes);
+void cargarCanciones(struct musica *canciones, short &cantCanciones);
+void parejasMayorDuracion(struct concursante concursantes[], short cantConcursantes, struct musica canciones[], short cantCanciones);
+void eliminarCancion(struct concursante concursantes[], short cantConcursantes, struct musica *canciones, short &cantCanciones);
+void duracionCancion(struct concursante concursantes[], short cantConcursantes, struct musica canciones[], short cantCanciones);
 
 int main() {
-  const short MAX_CONCURSANTES = 40;
+  const short MAX = 40;
   short cantConcursantes = 0;
-  concursante concursantes[MAX_CONCURSANTES];
+  short cantCanciones = 0;
+  char opcion;
+  musica canciones[MAX];
+  concursante concursantes[MAX];
 
-  cargaConcursantes(concursantes, cantConcursantes);
+  do {
+    cout << "Seleccione una opcion:" << std::endl;
+    cout << "[1] : Cargar nuevos concursantes." << std::endl;
+    cout << "[2] : Cargar nueva cancion." << std::endl;
+    cout << "[3] : Buscar cuantas parejas tienen la cancion mas larga." << std::endl;
+    cout << "[4] : Eliminar cancion." << std::endl;
+    cout << "[5] : Buscar duracion de la cancion por pareja." << std::endl;
+    cout << "[6] : Salir." << std::endl;
+    cin >> opcion;
+
+    switch (opcion) {
+      case '1':
+        cargaConcursantes(concursantes, cantConcursantes);
+        break;
+
+      case '2':
+        cargarCanciones(canciones, cantCanciones);
+        break;
+
+      case '3':
+        parejasMayorDuracion(concursantes, cantConcursantes, canciones, cantCanciones);
+        break;
+
+      case '4':
+        eliminarCancion(concursantes, cantConcursantes, canciones, cantCanciones);
+        break;
+
+      case '5':
+        duracionCancion(concursantes, cantConcursantes, canciones, cantCanciones);
+        break;
+
+      case '6':
+        break;
+      default:
+        cout << "Opcion invalida." << std::endl;
+        break;
+    }
+  }while (opcion != '6');
 
 
   return 0;
@@ -36,7 +86,7 @@ void cargaConcursantes(struct concursante *concursantes, short &cantConcursantes
   char opcionEmp;
 
   do {
-    string nombre1Temp = ""; string nombre2Temp = ""; string cancionTemp = "";
+    string nombre1Temp = ""; string nombre2Temp = ""; string cancionTemp = ""; string cancionFTemp = "";
     int numInscTemp = 0;
 
     cout << "Ingrese el nombre del primer integrante: ";
@@ -51,7 +101,9 @@ void cargaConcursantes(struct concursante *concursantes, short &cantConcursantes
     cout << "Ingrese el numero de inscripcion: ";
     cin >> numInscTemp;
 
-    if(cantConcursantes == 0) concursantes[0] = concursante(nombre1Temp, nombre2Temp, cancionTemp, numInscTemp);
+    cancionFTemp = formatearCancion(cancionTemp);
+
+    if(cantConcursantes == 0) concursantes[0] = concursante(nombre1Temp, nombre2Temp, cancionFTemp, numInscTemp);
     else {
       for(short i = 0; i < cantConcursantes; i++){ 
         if(concursantes[i].numInsc > numInscTemp) {
@@ -63,10 +115,10 @@ void cargaConcursantes(struct concursante *concursantes, short &cantConcursantes
                 concursantes[j].numInsc
                 );
           }
-          concursantes[i] = concursante(nombre1Temp, nombre2Temp, cancionTemp, numInscTemp);
+          concursantes[i] = concursante(nombre1Temp, nombre2Temp, cancionFTemp, numInscTemp);
           break;
         }
-        else concursantes[cantConcursantes] = concursante(nombre1Temp, nombre2Temp, cancionTemp, numInscTemp);
+        else concursantes[cantConcursantes] = concursante(nombre1Temp, nombre2Temp, cancionFTemp, numInscTemp);
       }
     }
     cantConcursantes++;
@@ -77,4 +129,102 @@ void cargaConcursantes(struct concursante *concursantes, short &cantConcursantes
 }
 
 //b)
+void cargarCanciones(struct musica *canciones, short &cantCanciones) {
+  char opcionEmp;
 
+  do {
+    string tituloTemp = "";
+    int duracionTemp = 0;
+
+    cout << "Ingrese el titulo de la cancion: ";
+    getline(cin>>ws, tituloTemp);
+
+    cout << "Ingrese cuanto dura la cancion en segundos: ";
+    cin >> canciones[cantCanciones].duracion;
+
+    canciones[cantCanciones].titulo = formatearCancion(tituloTemp);
+
+    cantCanciones++;
+    cout << "Cancion cargada correctamente." << std::endl;
+    cout << "¿Desea cargar otra cancion? (S/n)" << std::endl;
+    cin >> opcionEmp;
+  }while (opcionEmp != 'N' && opcionEmp != 'n');
+}
+
+string formatearCancion(string cancion) {
+  string cancionF = "";
+  for(int i = 0; i < cancion.length(); i++) {
+    if (i == 0) cancionF += toupper(cancion[i]);
+    else cancionF += tolower(cancion[i]);
+  }
+  return cancionF;
+}
+
+//e)
+void duracionCancion(struct concursante concursantes[], short cantConcursantes, struct musica canciones[], short cantCanciones) {
+  int parejaBuscada, tiempoEncontrado, duracionCancion;
+  cout << "Ingrese el numero de la pareja: ";
+  cin >> parejaBuscada;
+
+  for(short i = 0; i < cantConcursantes; i++) {
+    if (concursantes[i].numInsc == parejaBuscada) {
+      for(short j = 0; j < cantCanciones; j++) {
+        if (canciones[j].titulo == concursantes[i].cancion) {
+          tiempoEncontrado = canciones[j].duracion;
+          cout << "La cancion de la pareja durara un total de " << tiempoEncontrado << " segundos" << std::endl;
+          return;
+        }
+      }
+    }
+  }
+
+  cout << "No se encontro la cancion" << std::endl;
+}
+
+//c) podria usar la funcion de arriba para hacer este punto, pero usaria 3 for en vez de 2... Tambien podria pensar otra forma de hacer la funcion de arriba
+void parejasMayorDuracion(struct concursante concursantes[], short cantConcursantes, struct musica canciones[], short cantCanciones) {
+  int mayor = canciones[0].duracion;
+  string tituloMayor = canciones[0].titulo;
+
+  for (short i = 0; i < cantCanciones; i++) {
+    if(canciones[i].duracion > mayor) {
+      mayor = canciones[i].duracion;
+      tituloMayor = canciones[i].titulo;
+    }
+  }
+
+  cout << "La cancion mas larga es " << tituloMayor << " con una duracion de " << mayor << " segundos." << std::endl;
+  cout << "Las parejas que usaran esta cancion son: " << std::endl;
+
+  for (short i = 0; i < cantConcursantes; i++) {
+    if(concursantes[i].cancion == tituloMayor) {
+      cout << "[*] " << concursantes[i].numInsc << std::endl;
+    }
+  }
+}
+
+void eliminarCancion(struct concursante concursantes[], short cantConcursantes, struct musica *canciones, short &cantCanciones) {
+  string cancionEliminar;
+  cout << "Ingrese la cancion a eliminar: ";
+  getline(cin>>ws, cancionEliminar);
+
+  for(short i = 0; i < cantConcursantes; i++) {
+    if(concursantes[i].cancion == cancionEliminar) {
+      cout << "Imposible eliminar esta cancion, sera utilizada por concursantes" << std::endl;
+      return;
+    }
+  }
+
+  for(short i = 0; i < cantCanciones; i++)  {
+    if(canciones[i].titulo == cancionEliminar) {
+      for(short j = i; j < cantCanciones; j++) {
+        canciones[j].titulo = canciones[j+1].titulo;
+        canciones[j].duracion = canciones[j+1].duracion;
+      }
+      cantCanciones--;
+      cout << "Cancion eliminada correctamente!" << std::endl;
+      return;
+    }
+  }
+  cout << "Cancion no encontrada." << std::endl;
+}
