b441742d16ee60ff85b5f84632cc4228ed527879
kochq <matukoch@live.com>
2023-11-12 20:33:10 -0300

    ej10 bien hecho

--- pi/unidad3/tp3/ej10.cpp
+++ pi/unidad3/tp3/ej10.cpp
@@ -9,6 +9,7 @@ struct Node {
 void addNodo(Node*& head, const std::string cadena);
 std::string minimizar(const std::string& cadena);
 void pedirDatos(Node*& head);
+void moverNodo(Node* antNode, Node* movNode, Node*& newList); // Saca un nodo de la lista y lo mete en otra
 void listaRepetida(Node*& headTodas, Node*& headRepetidas);
 void mostrarLista(Node* head);
 
@@ -23,6 +24,8 @@ int main() {
   listaRepetida(head, headRepetidas);
   std::cout << "Lista2: " << std::endl;
   mostrarLista(headRepetidas);
+
+  return 0;
 }
 
 void addNodo(Node*& head, const std::string cadena) {
@@ -54,6 +57,35 @@ void pedirDatos(Node*& head) {
   }
 }
 
+void moverNodo(Node* antNode, Node* movNode, Node*& newList) { // Saca un nodo de la lista y lo mete en otra
+  Node* tMovNode = antNode->next; 
+
+  if(newList == nullptr) {
+    newList = movNode;
+
+    // Retiramos nodo de la lista anterior
+    if(movNode->next != nullptr) antNode->next = movNode->next;
+    else antNode->next = nullptr;
+
+    // Desenlazamos de la lista anterior
+    tMovNode->next = nullptr;
+  } 
+  else {
+    // Retiramos nodo de la lista anterior
+    if(movNode->next != nullptr) antNode->next = movNode->next;
+    else antNode->next = nullptr;
+
+    Node* tNode = newList;
+    while(tNode != nullptr) { // Iteramos para saber donde meter el nuevo nodo
+      if(tNode->next == nullptr) {
+        tNode->next = tMovNode; // Enlazamos nuevo nodo
+        tMovNode->next = nullptr; // Desenlazamos de la lista anterior
+      }
+      else tNode = tNode->next;
+    }
+  }
+}
+
 // Esto crea otra lista con todas las palabras repetidas
 // En esta lista las palabras que se repiten en la otra se repiten la misma cantidad de veces, 
 // la consigna no dice que eso este mal
@@ -65,8 +97,8 @@ void listaRepetida(Node*& headTodas, Node*& headRepetidas) {
     datoBuscado = headTodas->dato;
     tNode = headTodas;
     while(tNode != nullptr && tNode->next != nullptr) {
-      if(tNode->next->dato == datoBuscado) addNodo(headRepetidas, datoBuscado);
-      tNode = tNode->next;
+      if(tNode->next->dato == datoBuscado) moverNodo(tNode, tNode->next, headRepetidas);
+      else tNode = tNode->next;
     }
     headTodas = headTodas->next;
   }
