edc9d8c256c5b4e5da4c5df0c84106fdab60a05f
kochq <matukoch@live.com>
2024-04-03 22:26:27 -0300

    Ej07 | Recursiva bien

--- estructuraDatos/tp02/ej07/ej07.go
+++ estructuraDatos/tp02/ej07/ej07.go
@@ -8,6 +8,7 @@ import (
 func main() {
     arreglo := []int{1,2,3,4,5,6}
     invertirarreglo.Recursiva(arreglo, 0, len(arreglo)-1)
+    fmt.Printf("%d", invertirarreglo.Recursiva2(arreglo))
     fmt.Printf("%d", arreglo)
     invertirarreglo.Iterativa(arreglo)
     fmt.Printf("%d", arreglo)
--- estructuraDatos/tp02/ej07/invertirArreglo/invertirArreglo.go
+++ estructuraDatos/tp02/ej07/invertirArreglo/invertirArreglo.go
@@ -1,5 +1,15 @@
 package invertirarreglo
 
+func Recursiva2(arr []int) []int {
+    if(len(arr) == 0) {
+        return arr
+    }
+
+    tmpArr := Recursiva2(arr[1:])
+
+    return append(tmpArr, arr[0])
+}
+
 func Recursiva(arr []int, inicio, fin int) {
     if inicio < fin {
         arr[inicio], arr[fin] = arr[fin], arr[inicio]
