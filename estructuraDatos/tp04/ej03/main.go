aba6f6f61b1cd564c1468e3a002322004c697438
kochq <matukoch@live.com>
2024-04-20 20:40:43 -0300

    Ahora si

--- estructuraDatos/tp04/ej03/main.go
+++ estructuraDatos/tp04/ej03/main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"estructuraDatos/tp04/ej01/queue"
+	"estructuraDatos/tp04/ej01/stack"
 	"fmt"
-    "estructuraDatos/tp04/ej01/stack"
 )
 
-func main() {
-    var numero int
-    stack := stack.Stack{}
-
+func ordenInverso(stack *stack.Stack, numero *int) {
     for i := 0; i<10; i++ {
         fmt.Printf("Numero %d:", i)
-        fmt.Scan(&numero)
-        stack.PushStack(numero)
+        fmt.Scan(numero)
+        stack.PushStack(*numero)
     }
 
     for i := 0; i<10; i++ {
         fmt.Printf("Numero: %d\n", stack.PopStack())
     }
+}
 
+func ordenNormal(queue *queue.Queue, numero *int) {
+    for i := 0; i<10; i++ {
+        fmt.Printf("Numero %d:", i)
+        fmt.Scan(numero)
+        queue.PushQueue(*numero)
+    }
+
+    for i := 0; i<10; i++ {
+        fmt.Printf("Numero: %d\n", queue.PopQueue())
+    }
+}
+
+func main() {
+    var numero int
+    stack := stack.Stack{}
+    queue := queue.Queue{}
+
+    ordenInverso(&stack, &numero)
+    ordenNormal(&queue, &numero)
 }
