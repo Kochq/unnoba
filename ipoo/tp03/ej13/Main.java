ede3abada6c49b1e646a4f83173949700c6d26f4
kochq <matukoch@live.com>
2024-09-26 14:20:42 -0300

    EJ13 TP03 | IPOO

---
+++ ipoo/tp03/ej13/Main.java
@@ -0,0 +1,23 @@
+package tp03.ej13;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Main {
+    static private List<String> getFilterOutput(List<String> lines, String filter) {
+        List<String> result = new ArrayList<>();
+
+        lines.stream()
+            .filter(s -> !s.contains(filter))
+            .forEach(result::add);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<String> lines = Arrays.asList("Java", "PHP", "C++");
+
+        System.out.println(getFilterOutput(lines, "PHP"));
+    }
+}
